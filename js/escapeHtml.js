f52f8619640a1dc9c5af817fcb059286ddf2eb7b
zhanjh <zhanjh@126.com>
2018-01-10 15:18:55 +0800

    add escapeHtml & tpl

---
+++ js/escapeHtml.js
@@ -0,0 +1,8 @@
+export function escapeHtml(str) {
+    return str.replace(/&/g, '&amp;') // first!
+        .replace(/>/g, '&gt;')
+        .replace(/</g, '&lt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;')
+        .replace(/`/g, '&#96;');
+}
