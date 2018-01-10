500ac3cf12776bc913a16a82ec4f8d8d6062a4df
zhanjh <zhanjh@126.com>
2018-01-10 19:45:37 +0800

    trans, tpl

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 export {type} from './js/type.js';
 export {toFormData} from './js/toFormData.js';
+export {escapeHtml} from './js/escapeHtml.js';
+export {tpl} from './js/tpl.js';
+export {trans} from './js/trans.js';
